058f8252220de807539cde7d471f3f5543453232
Maxim Mashnin <onemillerlol@yandex.ru>
2021-12-15 20:24:50 +0500

    Недоделанная страница все преподаватели

---
+++ src/Components/Instructor/Instructor.jsx
@@ -0,0 +1,20 @@
+import './instructor.css';
+const Instructor = () => {
+    return (
+        <div>
+            <div className="wrapper">
+                <div className="wrapper__instructor">
+                    <div className="wrapp">
+                        <div className="block-left">
+                            <div className="instructor">
+                                Богатырев Михаил
+                            </div>
+                        </div>
+                        <div className="block-right">sadsadsad</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default Instructor
---
+++ src/pages/InstructorPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Header from '../Components/Header/Header';
+import Footer from '../Components/Footer/Footer';
+
+import Instructor from "../Components/Instructor/Instructor"
+const Instructors = () => {
+    return (
+        <div>
+            <Header/>
+            <Instructor/>
+            <Footer/>
+        </div>
+    );
+};
+export default Instructors;
